a4ccbfcae139e87cc61d64196bfd86f29ad23e45
Sergey <33185046+Trickster2038@users.noreply.github.com>
2020-10-02 23:55:00 +0300

    lab63_lambda_proc_block_solved

--- IP/lab6/lab6-3/src.rb
+++ IP/lab6/lab6-3/src.rb
@@ -1,9 +1,11 @@
-def maxim(func1, func2, start, stop, step_size)
+def maxim(func2, start, stop, step_size, &func1)
     max_distance = 0
     (start..stop).step(step_size) {|x| max_distance = (func1.call(x) - func2.call(x)).abs \
      if (func1.call(x) - func2.call(x)).abs > max_distance}
     max_distance
 end
 pr = lambda{|x| x*2}
+pr2 = lambda{|x| x*4}
 fr = proc{|x| x*3}
-p maxim(fr,pr, 0.5, 1, 0.01)
+p maxim(fr, 0.5, 1, 0.01){|x| x*2}
+p maxim(fr, 0.5, 1, 0.01, &pr2)
