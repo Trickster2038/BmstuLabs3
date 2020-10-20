08b6d59a658176f28a77f5eba12fff362467cdc9
LaptopSSDacc <fzastahov@gmail.com>
2020-10-20 09:23:41 +0300

    counter_shorted

--- IP/lab5/lab5-1/count.rb
+++ IP/lab5/lab5-1/count.rb
@@ -4,8 +4,6 @@
 # counts given function
 module Counter
   def self.count(x, y, z)
-    b1 = (1 + Math.cos(y.to_f - 2))
-    b2 = ((x.to_f**4)/2 + (Math.sin(z.to_f)**2))
-    res = b1.to_f / b2
+    res = (1 + Math.cos(y.to_f - 2)) / ((x.to_f**4)/2 + (Math.sin(z.to_f)**2))
   end
 end
