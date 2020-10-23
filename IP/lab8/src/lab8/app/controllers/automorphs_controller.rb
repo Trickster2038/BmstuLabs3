ec2016a9f3b16317a55c0235790099c9fdac6537
LaptopSSDacc <fzastahov@gmail.com>
2020-10-23 23:06:22 +0300

    lab8_no_tests

--- IP/lab8/src/lab8/app/controllers/automorphs_controller.rb
+++ IP/lab8/src/lab8/app/controllers/automorphs_controller.rb
@@ -3,49 +3,34 @@ class AutomorphsController < ApplicationController
 	end
 
 	def square?(num)
-  	# (1..(num/2+1)).each do |elem|
-  	# 	return true if elem**2 == num
-  	# end
-  	# return false
-  	return ((Math.sqrt(num)-Math.sqrt(num).round).abs < 10e-5)
-  end
+		return ((Math.sqrt(num)-Math.sqrt(num).round).abs < 10e-5)
+	end
 
-  def parse_sequence(list)
-  	result = []
-  	tail = list
-  	while tail.any? do 
-  		result.append(tail.take_while{|elem| square?(elem)})
-  		tail = tail.drop_while{|elem| square?(elem)}.drop_while{|elem| !square?(elem)}
-  	end
-  	#result[0] = []
-  	# slice_n = -1
-  	# sequence 
-  	# array.each do |elem|
-  	# 	if elem.square? 
-  	# 		if !sequence
-  	# 			slice_n = slice_n + 1 
-  	# 			result[slice_n] = []
-  	# 			sequence = true
-  	# 		end
-  	# 		result[slice_n] << elem
-  	# 	else
-  	# 		sequence = false
-  	# 	end
-  	# end
-  	return result
-  end
+	def parse_sequence(list)
+		result = []
+		tail = list
+		while tail.any? do 
+			result.append(tail.take_while{|elem| square?(elem)})
+			tail = tail.drop_while{|elem| square?(elem)}.drop_while{|elem| !square?(elem)}
+		end
+		return result
+	end
 
-  def parse_input(input_string)
-  	input_string.strip.split.map{|elem| elem.to_i}
-  end
+	def parse_input(input_string)
+		input_string.strip.split.map{|elem| elem.to_i}
+	end
 
 
 
-  def view
-     args = parse_input(params[:search_range])
-     @result = parse_sequence(args)
-     @result = "no such sequences to output[]" if @result.empty?
-  #     elem.to_s[-1] == (elem**2).to_s[-1]
+	def view
+		input_s = params[:search_range]
+		if input_s.match?(/[^0-9 ]/)
+			@result = "incorrect input"
+		else
+			@result = parse_sequence(parse_input(input_s))
+			@result = "no such sequences to output[]" if @result.flatten.empty?
+		end
+
 	end
 
 end
