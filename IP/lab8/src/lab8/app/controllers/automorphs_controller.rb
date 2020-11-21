c06f19f53c84f9412eaec32c5f7e99033c46f677
LaptopSSDacc <fzastahov@gmail.com>
2020-11-21 20:53:40 +0300

    lab8 update controller

--- IP/lab8/src/lab8/app/controllers/automorphs_controller.rb
+++ IP/lab8/src/lab8/app/controllers/automorphs_controller.rb
@@ -7,11 +7,11 @@ class AutomorphsController < ApplicationController
   def view
     input_s = params[:search_range]
     # checking only creation of views by default raises error of nil input
-    # коды ошибок вместо контсант
     if input_s.nil?
       res = 'error 1'
     elsif input_s.match?(/[^0-9 ]/)
       res = 'error 2'
+      #res = input_s.to_s
     else
       res = parse_sequence(parse_input(input_s))
       res = 'error 3' if res.flatten.empty?
@@ -26,10 +26,10 @@ def square?(num)
 end
 
 def parse_sequence(list)
-list.map{|elt| [elt,square?(elt)]}
-.slice_when{|elt1,elt2| elt1.last != elt2.last}
-.select{|grp| grp.first.last}
-.map{|grp| grp.map{|elt| elt.first}}
+  list.map { |elt| [elt, square?(elt)] }
+      .slice_when { |elt1, elt2| elt1.last != elt2.last }
+      .select { |grp| grp.first.last }
+      .map { |grp| grp.map(&:first) }
 end
 
 def parse_input(input_string)
