731dd405fc1e16e3fc3e27702fadb85b5a5babe7
LaptopSSDacc <fzastahov@gmail.com>
2020-12-02 22:15:55 +0300

    lab12 scaffold init + db:dev:migrate

--- IP/lab12/db/schema.rb
+++ IP/lab12/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_182246) do
+ActiveRecord::Schema.define(version: 2020_12_02_190730) do
 
   create_table "cached_results", force: :cascade do |t|
     t.string "input"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_12_01_182246) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["input"], name: "index_cached_results_on_input", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["login"], name: "index_users_on_login", unique: true
   end
 
 end
