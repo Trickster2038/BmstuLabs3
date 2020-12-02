f54494b3fe9d18abd104c72a0f488f62701d1ac7
LaptopSSDacc <fzastahov@gmail.com>
2020-12-02 17:52:07 +0300

    lab11(in work) uniq test

--- IP/lab11/test/models/cached_result_test.rb
+++ IP/lab11/test/models/cached_result_test.rb
@@ -3,5 +3,18 @@ require 'test_helper'
 class CachedResultTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
-  # end
+  # 
+  test 'should error on non unique record' do
+    instance1 = CachedResult.new()
+    instance1.input = "1"
+    instance1.result = "[[1]]"
+    instance1.save!
+    #instance.save!
+    instance2 = instance1.dup
+    #instance2.validate
+    assert_raise do 
+    	instance2.save!
+    end
+    #assert_equal false, instance2.valid?#instance.errors[:input], ['has already been taken']
+  end
 end
