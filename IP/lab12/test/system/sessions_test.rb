8bf5a90bc819314561de4c46df30742ce7db6bb1
LaptopSSDacc <fzastahov@gmail.com>
2020-12-03 18:00:40 +0300

    lab12 - integration tests

--- IP/lab12/test/system/sessions_test.rb
+++ IP/lab12/test/system/sessions_test.rb
@@ -17,4 +17,65 @@ class SessionsTest < ApplicationSystemTestCase
 	end
 
 
+  test 'register and count' do
+    login = "testCount@mail.ru"
+    password = "testpass"
+
+    visit signup_url
+
+
+    fill_in 'Email', with: login
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    click_on 'Create User'
+
+    assert_text 'Sessions#new'
+
+
+    fill_in 'Email', with: login
+    fill_in 'Password', with: password
+    click_on 'Login'
+
+    assert_text 'input'
+
+    fill_in 'search_range', with: 4
+    click_on 'Output result'
+
+    assert_text '4'
+    assert_text 'longest sequence:'
+
+
+    # click_on 'Logout'
+
+    # assert_text 'Login'
+  end
+
+  test 'register login and logout' do
+  	login = "testCount@mail.ru"
+    password = "testpass"
+
+    visit signup_url
+
+
+    fill_in 'Email', with: login
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    click_on 'Create User'
+
+    assert_text 'Sessions#new'
+
+
+    fill_in 'Email', with: login
+    fill_in 'Password', with: password
+    click_on 'Login'
+
+    assert_text 'input'
+
+    click_on 'Logout'
+    assert_text 'Login'
+  end
+
+
+
+
 end
