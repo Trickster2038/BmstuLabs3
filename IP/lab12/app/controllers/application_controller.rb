37db46de89e510be0ec6005fae43d4db2b6b2174
LaptopSSDacc <fzastahov@gmail.com>
2020-12-03 14:15:36 +0300

    lab12(no tests) done

--- IP/lab12/app/controllers/application_controller.rb
+++ IP/lab12/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  before_action :require_auth, except: [:new, :edit, :destroy, :create, :show, :index]
+  before_action :require_auth, except: [:new, :create]
+  # :edit, :destroy, :show, :index
 
   def current_user
     if session[:user_id]
